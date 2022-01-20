8914e17393be3709a7e4173174bab32594997233
Mhairi <mhairi@thetradetoolbox.com>
2022-01-20 21:52:55 +0000

    checking set up with basic "Hello World" component

---
+++ src/App/Commands/HelloWorldCommand.php
@@ -0,0 +1,28 @@
+<?php
+namespace Console\App\Commands;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * Class HelloWorld
+ *
+ * @package App\Command
+ */
+class HelloWorldCommand extends Command
+{
+    protected function configure()
+    {
+        $this
+            ->setName('app:hello-world')
+            ->setDescription('Outputs "Hello World"');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $output->writeln("Hello World");
+
+        return Command::SUCCESS;
+    }
+}
